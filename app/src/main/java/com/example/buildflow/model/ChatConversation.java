0a977b0a9a6c9192e7d5124c1b207126139b57b4
sariel <sarielmoshe1@gmail.com>
2026-02-26 18:06:15 +0200

    comments

--- app/src/main/java/com/example/buildflow/model/ChatConversation.java
+++ app/src/main/java/com/example/buildflow/model/ChatConversation.java
@@ -1,37 +1,37 @@
-package com.example.buildflow.model;
-
-/**
- * a class for the chat conversation
- */
-public class ChatConversation {
-    private String id;
-    private String name;
-    private String role;
-    private String lastMessage;
-    private String time;
-    private int unreadCount;
-    private boolean isOnline;
-    public ChatConversation() {
-        // Default constructor required for Firebase.
-    }
-
-    public ChatConversation(String id, String name, String role, String lastMessage, String time, int unreadCount, boolean isOnline) {
-        this.id = id;
-        this.name = name;
-        this.role = role;
-        this.lastMessage = lastMessage;
-        this.time = time;
-        this.unreadCount = unreadCount;
-        this.isOnline = isOnline;
-    }
-
-    //  Getters
-    public String getName() { return name; }
-    public String getRole() { return role; }
-    public String getLastMessage() { return lastMessage; }
-    public String getTime() { return time; }
-    public int getUnreadCount() { return unreadCount; }
-    public boolean isOnline() { return isOnline; }
-    public String getId() { return id; }
-
-}
+//package com.example.buildflow.model;
+//
+///**
+// * a class for the chat conversation
+// */
+//public class ChatConversation {
+//    private String id;
+//    private String name;
+//    private String role;
+//    private String lastMessage;
+//    private String time;
+//    private int unreadCount;
+//    private boolean isOnline;
+//    public ChatConversation() {
+//        // Default constructor required for Firebase.
+//    }
+//
+//    public ChatConversation(String id, String name, String role, String lastMessage, String time, int unreadCount, boolean isOnline) {
+//        this.id = id;
+//        this.name = name;
+//        this.role = role;
+//        this.lastMessage = lastMessage;
+//        this.time = time;
+//        this.unreadCount = unreadCount;
+//        this.isOnline = isOnline;
+//    }
+//
+//    //  Getters
+//    public String getName() { return name; }
+//    public String getRole() { return role; }
+//    public String getLastMessage() { return lastMessage; }
+//    public String getTime() { return time; }
+//    public int getUnreadCount() { return unreadCount; }
+//    public boolean isOnline() { return isOnline; }
+//    public String getId() { return id; }
+//
+//}
